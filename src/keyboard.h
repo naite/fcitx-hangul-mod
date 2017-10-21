a27e32a9245566ccce7454233189d392e1030ecf
naite <dervish.fp@gmail.com>
2017-10-22 00:11:38 +0900

    Add README.md

---
+++ src/keyboard.h
@@ -0,0 +1,50 @@
+/* generated */
+
+#ifndef FCITX_HANGUL_KEYBOARD_H
+#define FCITX_HANGUL_KEYBOARD_H
+
+typedef enum _FcitxHangulKeyboard
+{
+    Dubeolsik_KSX_5002 = 0,
+    Dubeolsik_Noshift,
+    Dubeolsik_North_9256,
+    Sebeolsik_3_90,
+    Sebeolsik_3_91_Final,
+    Sebeolsik_3_P3,
+    Sebeolsik_Semoe_2015,
+    Sebeolsik_Shin_P,
+    Sebeolsik_Shin_P2,
+    Dubeolsik_Yetgeul,
+    Sebeolsik_Dubeol_Layout,
+    Sebeolsik_3_89,
+    Sebeolsik_3_91_Final_Noshift,
+    Sebeolsik_3_93_Yetgeul,
+    Romaja,
+    Ahnmatae,
+    Sebeolsik_3Ahnmatae,
+    Sebeolsik_3_2011,
+    Sebeolsik_3_2011_yet,
+    Sebeolsik_3_2012,
+    Sebeolsik_3_2012_yet,
+    Sebeolsik_3_2014,
+    Sebeolsik_3_2014_yet,
+    Sebeolsik_3_2015,
+    Sebeolsik_3_2015_yet,
+    Sebeolsik_3_2015M,
+    Sebeolsik_3_2015P,
+    Sebeolsik_3_2015P_yet,
+    Sebeolsik_3_14_Proposal,
+    Sebeolsik_Semoe_2014,
+    Sebeolsik_Noshift_1990,
+    Sebeolsik_Noshift_2014,
+    Sebeolsik_3Gimguk_38A_Yetgeul,
+    Sebeolsik_Shin_1995,
+    Sebeolsik_Shin_2003,
+    Sebeolsik_Shin_2012,
+    Sebeolsik_Shin_2015,
+    Sebeolsik_Shin_M,
+} FcitxHangulKeyboard;
+
+extern const char* keyboard[];
+
+#endif
